172d1d9137b107e5e2915f6d735b9776b28ec088
Matt Herzog <68869045+mherzog4@users.noreply.github.com>
2022-09-30 10:21:38 -0400

    Time: 163 ms (36.65%), Space: 42.4 MB (74.21%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,12 +3,26 @@
  * @param {number} target
  * @return {number[]}
  */
+
+/*
+nums = [1,2,3]
+target = 4
+[0,2]
+*/
+
+
 var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
+    for( let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                return [i, j]
+            if(nums[i] + nums[j] == target) {
+                return [i,j]
             }
         }
     }
 };
+
+
+
+
+
+
