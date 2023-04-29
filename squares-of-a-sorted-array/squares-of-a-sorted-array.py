2df3dbcad06a8dd9adc82e1c90993cb2774c46d5
Matt Herzog <68869045+mherzog4@users.noreply.github.com>
2023-04-29 14:12:58 -0400

    Time: 222 ms, Memory: 18.9 MB - LeetHub

---
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n -1
+        for i in range(n - 1, -1, -1):
+            if abs(nums[left]) < abs(nums[right]):
+                square = nums[right]
+                right -= 1
+            else:
+                square = nums[left]
+                left += 1
+            result[i] = square * square
+        return result
+            
+            
+        
+        
+# Complexity Analysis
+
+# Time Complexity: 
+# O(N), where N is the length of A.
+
+# Space Complexity: 
+# O(N) if you take output into account and O(1) otherwise.
